d135339ffc4625c73e69b08418068e00be9fa039
cangle <cang.b.le@gmail.com>
2023-06-23 01:28:27 +0700

    first commitment

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,3 @@
-// import logo from "./logo.svg";
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-/////
-
 import React, { Suspense } from "react";
 import { Provider } from "react-redux";
 import setupStore from "./redux/setupStore";
@@ -42,7 +14,7 @@ const App = (props) => {
   return (
     <div className="App">
       <Provider store={store}>
-        <Suspense>
+        <Suspense fallback={<Loader />}>
           <Router history={history}>
             <Routes>
               <Route exact path="/" element={<HomePageContainer />} />
